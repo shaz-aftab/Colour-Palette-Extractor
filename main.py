d9ef75a45bbb531f58713d58a38fc87f4d001ec3
shaz-aftab <shazaftab20@gmail.com>
2025-08-25 17:15:19 +0100

    displays now using matplotlib

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import cv2 as cv
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
 
-# def normalize(*args):
-#     for i in range(1, len(args)):
-#         return args[i]/255
-
 scaler = preprocessing.MinMaxScaler()
 
 # Read, Convert & Flatten 
@@ -16,5 +12,24 @@ img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 pixels = scaler.fit_transform(img_rgb.reshape((-1, 3)))
 
 # Clustering
-kmeans = KMeans(n_clusters=5, random_state=42)
+k = 5
+kmeans = KMeans(n_clusters=k, random_state=42)
 kmeans.fit(pixels)
+
+colours_normalized = kmeans.cluster_centers_
+
+h, w = 50, 50 * k
+
+strip = np.zeros((h, w, 3))
+
+for i, colour in enumerate(colours_normalized):
+    StartX = i * 50
+    EndX = (i + 1) * 50
+    strip[:, StartX:EndX, :] = colour
+
+# Display
+
+plt.figure(figsize=(8, 2))
+plt.imshow(strip)
+plt.axis("off")
+plt.show()
